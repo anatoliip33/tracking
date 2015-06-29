c38ec169ee4589c382fa38f8d13026ffb0284bd3
Anatolii Polischuk <anatoliipolishchuk@gmail.com>
2015-06-29 19:43:35 +0300

    refactoring ticket code

--- app/models/concerns/codable.rb
+++ app/models/concerns/codable.rb
@@ -3,8 +3,12 @@ require 'active_support/concern'
 module Codable
   extend ActiveSupport::Concern
 
-  def random_digits(n)
-    (0..9).cycle(n).to_a.shuffle[0, n].join
+  # def random_digits(n)
+  #   (0..9).cycle(n).to_a.shuffle[0, n].join
+  # end
+
+  def random_hex
+    rand(256).to_s(16).rjust(2, '0').upcase
   end
 
   def random_letters(n)
@@ -12,7 +16,7 @@ module Codable
   end
 
   def random_code
-    random_letters(3) + random_digits(3) + random_letters(3) + random_digits(3) + random_letters(3)
+    random_letters(3) + '-' + random_hex + '-' + random_letters(3) + '-' + random_hex + '-' + random_letters(3)
   end
 
   def generate_code
